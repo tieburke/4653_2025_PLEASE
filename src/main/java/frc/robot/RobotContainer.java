74f6c6aca494a22e9a1bd63cbbd7544a2aa6a192
tieburke <92586868+tieburke@users.noreply.github.com>
2025-03-11 10:15:15 -0400

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -148,37 +148,28 @@ public class RobotContainer {
 
     /* Limelight Stuff */
 
-    // simple proportional turning control with Limelight.
-    // "proportional control" is a control algorithm in which the output is proportional to the error.
-    // in this case, we are going to return an angular velocity that is proportional to the 
-    // "tx" value from the Limelight.
-    public static double limelight_aim_proportional(){    
-        // kP (constant of proportionality)
-        // this is a hand-tuned number that determines the aggressiveness of our proportional control loop
-        // if it is too high, the robot will oscillate.
-        // if it is too low, the robot will never reach its target
-        // if the robot never turns in the correct direction, kP should be inverted.
+    public static double limelight_aim_proportional(String leftRight){    
         double kP = -.01;
-    
-        // tx ranges from (-hfov/2) to (hfov/2) in degrees. If your target is on the rightmost edge of 
-        // your limelight 3 feed, tx should return roughly 31 degrees.
-        double targetingAngularVelocity = LimelightHelpers.getTX("limelight") * kP;
-    
-        // convert to radians per second for our drive method
+        if(leftRight = "left"){
+            double targetingAngularVelocity = LimelightHelpers.getTX("limelightL") * kP;
+        }
+        else{
+            double targetingAngularVelocity = LimelightHelpers.getTX("limelightR") * kP;
+        }
         targetingAngularVelocity *= Constants.Swerve.maxAngularVelocity;
-    
-        //invert since tx is positive when the target is to the right of the crosshair
         targetingAngularVelocity *= -0.05;
-    
         return targetingAngularVelocity;
     }
     
-    // simple proportional ranging control with Limelight's "ty" value
-    // this works best if your Limelight's mount height and target mount height are different.
-    // if your limelight and target are mounted at the same or similar heights, use "ta" (area) for target ranging rather than "ty"
-    public static double limelight_range_proportional(){    
+    public static double limelight_range_proportional(String leftRight){    
         double kP = .01;
-        double targetingForwardSpeed = LimelightHelpers.getTY("limelight") * kP;
+        if(leftRight = "left"){
+            double targetingForwardSpeed = LimelightHelpers.getTY("limelightL") * kP;
+        }
+        else{
+            double targetingForwardSpeed = LimelightHelpers.getTY("limelightR") * kP;
+        }
+        //double targetingForwardSpeed = LimelightHelpers.getTY("limelight") * kP;
         targetingForwardSpeed *= Math.PI;
         targetingForwardSpeed *= -0.05;
         return targetingForwardSpeed;
